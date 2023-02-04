2004cf6c25c760cfc55bc447a41c0011cd2d9916
Rinat <rinat@kloop.kg>
2023-02-04 19:09:21 +0600

    Start adding tests (BUGGY!!!)

---
+++ dance/tests/test_answersheet.py
@@ -0,0 +1,6 @@
+from rest_framework.test import APITestCase
+
+
+
+class AnswerSheetTest(APITestCase):
+    def setUp(self) -> None:
---
+++ dance/utils/csv_to_words_converter.py
@@ -0,0 +1,26 @@
+import csv
+
+from dance.const import Languages
+from dance.models import Word
+
+
+def convert(
+    vocab_string,
+    skip_first_line=True,
+    translations_included=False,
+    primary_language=Languages.ENGLISH,
+):
+    vocab_list = list(csv.reader(vocab_string.splitlines()))
+    if skip_first_line:
+        vocab_list.pop(0)
+
+    for word in vocab_list:
+        new_word, created = Word.objects.get_or_create(
+            defaults={"order": word[2]}, word=word[0], language=word[1]
+        )
+        if translations_included:
+            word_to_translate = None
+            if new_word.language == primary_language:
+                word_to_translate = new_word
+            elif word_to_translate is not None:
+                word_to_translate.translations.add(new_word)
